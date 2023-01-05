7c0fa399ef8962bde33e594cf06b30fa081d2b2c
Why so serious? <yodfha2014@gmail.com>
2023-01-05 17:41:35 +0700

    implement graceful shutdown

--- server.go
+++ server.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"log"
+	"context"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -22,5 +26,20 @@ func main() {
 	e.PUT("/expenses/:id", expense.UpdateExpenseHandler)
 	e.GET("/expenses", expense.GetExpensesHandler)
 
-	log.Fatal(e.Start(os.Getenv("PORT")))
+	go func() {
+		if err := e.Start(os.Getenv("PORT")); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	<-shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+
 }
